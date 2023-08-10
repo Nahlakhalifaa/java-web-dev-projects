266ef4862607ade9db30a5c7bdee633c64b7c69e
Nahlakhalifaa <nahla.kh0025@gmail.com>
2023-08-09 19:38:03 -0500

    My firt comme

--- datatypes/datatypes-studio/src/org/launchcode/studios/Area.java
+++ datatypes/datatypes-studio/src/org/launchcode/studios/Area.java
@@ -5,12 +5,13 @@ import org.w3c.dom.ls.LSOutput;
 import java.util.Scanner;
 
 public class Area {
-    public static void main(String Arg[]){
+    public static void main(String Arg[]) {
         System.out.println("Enter a radius:");
         Scanner input = new Scanner(System.in);
-        double radius=input.nextDouble();
-//        System.out.println(3.14 *radius*radius);
+        double radius = input.nextDouble();
+//      System.out.println(3.14 *radius*radius);
         System.out.println(circle.getArea(radius));
+
     }
 }
 
